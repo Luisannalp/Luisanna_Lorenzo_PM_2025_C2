9b9005a96633af546e236019028dcc48aa9192df
Luisannalp <Paulinoluisanna02@gmail.com>
2025-05-21 22:18:04 -0400

    Add files via upload

---
+++ Ejercicios/Programas/Ejercicio 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Programa 1.1
+El siguiente es el primer programa escrito en el lenaguje C. */
+
+void main(void)
+{
+    printf("Mi primer programa en C");
+}
