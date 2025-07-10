f123ad3ab4f3102f0e29a8094678c768da98974e
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:23:30 -0400

    Update main.c

--- Programas.Cap.5/Programa.5.11/main.c
+++ Programas.Cap.5/Programa.5.11/main.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-
-/* Búsqueda binaria en un arreglo ordenado en forma creciente. */
-
-#define MAX 100  // Definición de tamaño máximo del arreglo
-
-// Prototipos de funciones
+#define MAX 100  // DefiniciÃ³n de tamaÃ±o mÃ¡ximo del arreglo
 void Lectura(int A[], int T);
 int Binaria(int A[], int T, int E);
 
 int main(void)
 {
     int RES, ELE, TAM, VEC[MAX];
-
-    // Verificar que el tamaño del arreglo sea válido
     do {
-        printf("Ingrese el tamaño del arreglo: ");
+        printf("Ingrese el tamaÃ±o del arreglo: ");
         scanf("%d", &TAM);
     } while (TAM > MAX || TAM < 1);
 
@@ -26,7 +19,7 @@ int main(void)
     RES = Binaria(VEC, TAM, ELE);
 
     if (RES)
-        printf("\nEl elemento se encuentra en la posición: %d\n", RES);
+        printf("\nEl elemento se encuentra en la posiciÃ³n: %d\n", RES);
     else
         printf("\nEl elemento no se encuentra en el arreglo\n");
 
@@ -34,7 +27,6 @@ int main(void)
 }
 
 void Lectura(int A[], int T)
-// La función Lectura se usa para llenar el arreglo con T elementos
 {
     int I;
     for (I = 0; I < T; I++) {
@@ -44,15 +36,14 @@ void Lectura(int A[], int T)
 }
 
 int Binaria(int A[], int T, int E)
-// Realiza búsqueda binaria del elemento E en arreglo A ordenado
 {
     int IZQ = 0, DER = T - 1, CEN;
-    int POS = 0;  // Guardará la posición encontrada (1-based) o 0 si no
+    int POS = 0;  
 
     while (IZQ <= DER && POS == 0) {
         CEN = (IZQ + DER) / 2;
         if (E == A[CEN])
-            POS = CEN + 1;  // Se suma 1 porque el usuario espera posición desde 1
+            POS = CEN + 1; 
         else if (E > A[CEN])
             IZQ = CEN + 1;
         else
