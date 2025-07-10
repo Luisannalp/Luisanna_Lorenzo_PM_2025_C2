d1b232e4ac3af5d345bc0249308d0f83c098d6c6
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:39:56 -0400

    Update main.c

--- Programas-cap-2/Programa2.2/main.c
+++ Programas-cap-2/Programa2.2/main.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/*Incremento de precio.
-El programa, al recibir como dato el precio de un producto importado,
-incrementa 11% el mismo si este es inferior a $1,500.
-
-PRE y NPR: variable de tipo real. */
-
 void main(void)
 {
     float PRE, NPR;
