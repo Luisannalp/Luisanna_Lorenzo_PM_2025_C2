ef89316b3a1b0719c76b688da563f7b836d0a3aa
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:35:22 -0400

    Update main.c

--- Programa1.4/main.c
+++ Programa1.4/main.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-
-/* Superficie del tri·ngulo.
-El programa, al recibir como datos la base y la altura de un tri·ngulo,
-calcula su perficie.
-
-BAS, ALT y SUP: variables de tipo real. */
-
 void main(void)
 {
     float BAS, ALT, SUP;
-    printf("Ingrese la base y la altura del tri·ngulo:");
+    printf("Ingrese la base y la altura del tri√°ngulo:");
     scanf("%f %f", &BAS, &ALT);
     SUP = BAS * ALT / 2;
-    printf("\nLa superficie del tri·ngulo es: %5.2f", SUP);
+    printf("\nLa superficie del tri√°ngulo es: %5.2f", SUP);
 }
