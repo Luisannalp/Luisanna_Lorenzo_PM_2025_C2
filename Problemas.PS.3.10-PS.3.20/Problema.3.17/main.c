a10d1d5253e90db62cf31f77afd1464b5e236cd7
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:25:43 -0400

    Update main.c

--- Problemas.PS.3.10-PS.3.20/Problema.3.17/main.c
+++ Problemas.PS.3.10-PS.3.20/Problema.3.17/main.c
@@ -2,16 +2,13 @@
 int main() {
     int N;
 
-    printf("Ingrese un número entero positivo: ");
+    printf("Ingrese un nÃºmero entero positivo: ");
     scanf("%d", &N);
 
-    // Generar la figura
     for (int i = 1; i <= N; i++) {
-        // Parte creciente (1 hasta i)
         for (int j = 1; j <= i; j++) {
             printf("%d ", j);
         }
-        // Parte decreciente (i-1 hasta 1)
         for (int j = i - 1; j >= 1; j--) {
             printf("%d ", j);
         }
