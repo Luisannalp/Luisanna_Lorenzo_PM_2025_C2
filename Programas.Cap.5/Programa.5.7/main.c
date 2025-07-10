be9411491528bd7df7647b56d041a2f83ac3dae9
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:33:23 -0400

    Update main.c

--- Programas.Cap.5/Programa.5.7/main.c
+++ Programas.Cap.5/Programa.5.7/main.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 
-/* Arreglo sin elementos repetidos.
-   El programa, al recibir como dato un arreglo unidimensional desordenado de N
-   elementos, obtiene como salida ese mismo arreglo pero sin los elementos
-   repetidos. */
-
-void Lectura(int *, int);           // Prototipos de funciones.
+void Lectura(int *, int);         
 void Imprime(int *, int);
 void Elimina(int *, int *);
-// Observa que en el prototipo de Elimina, el segundo parámetro es por
-// referencia. Esto, porque el tamaño del arreglo puede disminuir.
-
 void main(void)
 {
     int TAM, ARRE[100];
-
-    // Se escribe un do-while para verificar que el tamaño del arreglo que se
-    // ingresa sea correcto.
     do {
-        printf("Ingrese el tamaño del arreglo: ");
+        printf("Ingrese el tamaÃ±o del arreglo: ");
         scanf("%d", &TAM);
     } while (TAM > 100 || TAM < 1);
 
     Lectura(ARRE, TAM);
-    Elimina(ARRE, &TAM);  // Observa que el tamaño del arreglo se pasa por referencia.
+    Elimina(ARRE, &TAM); 
     Imprime(ARRE, TAM);
 }
 
 void Lectura(int A[], int T)
-// La función Lectura se utiliza para leer un arreglo unidimensional de T
-// elementos de tipo entero.
 {
     printf("\n");
     int I;
@@ -41,8 +28,6 @@ void Lectura(int A[], int T)
 }
 
 void Imprime(int A[], int T)
-// La función Imprime se utiliza para escribir un arreglo unidimensional, sin
-// repeticiones, de T elementos de tipo entero.
 {
     int I;
     for (I = 0; I < T; I++)
@@ -50,8 +35,6 @@ void Imprime(int A[], int T)
 }
 
 void Elimina(int A[], int *T)
-// Esta función se utiliza para eliminar los elementos repetidos de un arreglo
-// unidimensional de T elementos de tipo entero.
 {
     int I = 0, K, L;
     while (I < (*T - 1))
@@ -63,7 +46,7 @@ void Elimina(int A[], int *T)
             {
                 for (L = K; L < (*T - 1); L++)
                     A[L] = A[L + 1];
-                *T = *T - 1;  // Disminuye el tamaño del arreglo.
+                *T = *T - 1; 
             }
             else
             {
