f0b0afc664e6b2a1fe00efff104de25452fed48c
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:26:45 -0400

    Update main.c

--- Problemas.PS.3.10-PS.3.20/Problema.3.19/main.c
+++ Problemas.PS.3.10-PS.3.20/Problema.3.19/main.c
@@ -6,27 +6,20 @@ int main() {
     scanf("%d", &N);
 
     for (int i = 1; i <= N; i++) {
-        // Espacios a la izquierda para centrar la pirámide
         for (int s = 0; s < N - i; s++) {
             printf("  ");
         }
-
-        // Parte ascendente
         int num = i;
         for (int j = 0; j < i; j++) {
             printf("%d ", num % 10);
             num++;
         }
-
-        // Parte descendente
         num -= 2;
         for (int j = 0; j < i - 1; j++) {
             printf("%d ", num % 10);
             num--;
         }
-
         printf("\n");
     }
-
     return 0;
 }
