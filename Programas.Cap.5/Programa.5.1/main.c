81069747552140220268f7cd93e0775e17e88d3d
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:22:04 -0400

    Update main.c

--- Programas.Cap.5/Programa.5.1/main.c
+++ Programas.Cap.5/Programa.5.1/main.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
-
-/* Cuenta-números.
-El programa, al recibir como datos un arreglo unidimensional de tipo
-entero y un número entero, determina cuántas veces se encuentra el
-número en el arreglo. */
-
 void main(void)
 {
     int I, NUM, CUE = 0;
-    int ARRE[100];                           /* Declaración del arreglo */
+    int ARRE[100];                        
     for (I=0; I<100; I++)
     {
         printf("Ingrese el elemento %d del arreglo: ", I+1);
-        scanf("%d", &ARRE[I]);            /* Lectura -asignación— del arreglo */
+        scanf("%d", &ARRE[I]);        
     }
-    printf("\n\nIngrese el número que se va a buscar en el arreglo:");
+    printf("\n\nIngrese el nÃºmero que se va a buscar en el arreglo:");
     scanf("%d", &NUM);
     for (I=0; I<100; I++)
-        if (ARRE[I] == NUM)  /* Comparación del número con los elementos del
-                             arreglo */
+        if (ARRE[I] == NUM) 
             CUE++;
     printf("\n\nEl %d se encuentra %d veces en el arreglo", NUM, CUE);
 }
