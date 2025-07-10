a8c4eb6549522a6e7994c0ce089ae06fbfc25940
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:25:16 -0400

    Update main.c

--- Problemas.PS.3.10-PS.3.20/Problema.3.15/main.c
+++ Problemas.PS.3.10-PS.3.20/Problema.3.15/main.c
@@ -2,27 +2,21 @@
 
 int main() {
     int N1, N2, temp;
-
-    // Pedir que ingrese dos números
-    printf("Ingrese el primer número (N1): ");
+    printf("Ingrese el primer nÃºmero (N1): ");
     scanf("%d", &N1);
-
-    printf("Ingrese el segundo número (N2): ");
+    printf("Ingrese el segundo nÃºmero (N2): ");
     scanf("%d", &N2);
 
-    // Validar que los números sean naturales (mayores o iguales a cero)
     if (N1 < 0 || N2 < 0) {
-        printf("Por favor ingrese números naturales (0 o mayores).\n");
-        return 1;  // Salir del programa con error
+        printf("Por favor ingrese nÃºmeros naturales (0 o mayores).\n");
+        return 1;  
     }
-
     while (N2 != 0) {
         temp = N2;
         N2 = N1 % N2;
         N1 = temp;
     }
-
-    printf("El máximo común divisor (MCD) es: %d\n", N1);
+    printf("El mÃ¡ximo comÃºn divisor (MCD) es: %d\n", N1);
 
     return 0;
 }
