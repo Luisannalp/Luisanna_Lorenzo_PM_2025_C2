d2c93b7cae24413397dce6131f08b6633f1d2cea
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:55:47 -0400

    Update main.c

--- Programas.Cap.4/Programa.4.11/main.c
+++ Programas.Cap.4/Programa.4.11/main.c
@@ -1,29 +1,18 @@
  # include <stdio.h>
-
- /* Mayor divisor.
-El programa, al recibir como dato un número entero positivo, calcula
- su mayor divisor. */
-
- int  mad(int);               /* Prototipo de función. */
+ int  mad(int); 
  void main(void)
  {
  int NUM, RES;
- printf("\nIngresa el número:");
+ printf("\nIngresa el nÃºmero:");
  scanf("%d", &NUM);
  RES = mad(NUM);
  printf("\nEl mayor divisor de %d es: %d", NUM, RES);
  }
  int mad(int N1)
- /* Esta función calcula el mayor divisor del número NN11. */
+
  {
       int  I = (N1 / 2);
-/* II se inicializa con el máximo valor posible que puede ser divisor
- de NN11. */
-
  while (N1 % I)
-/* El ciclo se mantiene activo mientras ((NN11  %%  II)) sea distinto de cero.
- Cuando el resultado sea 0, se detiene, ya que se habrá encontrado
- el mayor divisor de NN11. */
  I--;
  return I;
  }
