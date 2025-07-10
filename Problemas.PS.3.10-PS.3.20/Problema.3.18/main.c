7c7552fbd5b64743dfa15deac107f15b7ace78cd
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:26:18 -0400

    Update main.c

--- Problemas.PS.3.10-PS.3.20/Problema.3.18/main.c
+++ Problemas.PS.3.10-PS.3.20/Problema.3.18/main.c
@@ -3,27 +3,21 @@
 int main() {
     int N;
 
-    printf("Ingrese un número entero positivo: ");
+    printf("Ingrese un nÃºmero entero positivo: ");
     scanf("%d", &N);
 
     for (int fila = 0; fila < N; fila++) {
-        // Números izquierdos (ascendentes)
         for (int num = 1; num <= N - fila; num++) {
             printf("%d ", num);
         }
-
-        // Espacios centrales
         int espacios = 2 * fila + 1;
         for (int e = 0; e < espacios; e++) {
-            printf("  "); // Dos espacios por posición
+            printf("  "); 
         }
-
-        // Números derechos (descendentes)
         for (int num = N - fila; num >= 1; num--) {
             printf("%d ", num);
         }
-
-        printf("\n"); // Cambio de línea
+        printf("\n"); 
     }
 
     return 0;
