332d1cf6237ecc6b5fdf9a9134368779197e6e4c
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:15:36 -0400

    Update main.c

--- Programas.Cap.4/Programa.4.3/main.c
+++ Programas.Cap.4/Programa.4.3/main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-/* Conflicto de variables con el mismo nombre. */
-
-int K_gl = 5;                /* Variable global. */
-
-void f1(void);            /* Prototipo de función. */
-
-
+int K_gl = 5;     
+void f1(void);      
 void main (void)
 {
     int I;
@@ -15,14 +10,12 @@ void main (void)
 }
 
 void f1(void)
-/* La función utiliza tanto la variable local I como la variable
-global I. */
 {
-    int K = 2;               /* Variable local. */
+    int K = 2;        
     K += K;
     printf("\n\nEl valor de la variable local es: %d", K);
 
-    extern int K_gl;     /* Variable global con nombre distinto. */
+    extern int K_gl;  
     K_gl = K_gl + K;
     printf("\nEl valor de la variable global es: %d", K_gl);
 }
