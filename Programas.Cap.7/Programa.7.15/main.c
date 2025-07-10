fbe36e7e3204f8df688d2a2a01711a8ee6642522
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:58:11 -0400

    Update main.c

--- Programas.Cap.7/Programa.7.15/main.c
+++ Programas.Cap.7/Programa.7.15/main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
-
-/* Decodifica.
-   El programa decodifica una cadena de caracteres compuesta por números y letras. */
-
-void interpreta(char *);  /* Prototipo de función */
+void interpreta(char *); 
 
 int main(void)
 {
@@ -15,16 +11,13 @@ int main(void)
 }
 
 void interpreta(char *cadena)
-/* Esta función se utiliza para decodificar la cadena de caracteres. */
 {
     int i = 0, j, k;
     while (cadena[i] != '\0')
     {
-        if (isalpha(cadena[i]))  /* Verifica si es una letra */
+        if (isalpha(cadena[i])) 
         {
             k = cadena[i - 1] - 48;
-            /* Se resta 48 al valor ASCII del dígito anterior para convertirlo a entero */
-
             for (j = 0; j < k; j++)
                 putchar(cadena[i]);
         }
