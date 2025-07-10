c864f7a282a5b9a72d48c1d15fd819f42ea26007
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:32:34 -0400

    Update main.c

--- Programas.Cap.5/Programa.5.6/main.c
+++ Programas.Cap.5/Programa.5.6/main.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
-/* Suma-cuadrados.
-   El programa calcula la suma del cuadrado de los elementos de un arreglo
-   unidimensional de 100 elementos de tipo real. */
-
 const int MAX = 100;
-// MAX se utiliza para reservar el espacio máximo que podrá ocupar el arreglo.
 
-void Lectura(float *, int);        // Prototipos de funciones.
+void Lectura(float *, int);     
 double Suma(float *, int);
 
 void main(void)
 {
     float VEC[MAX];
     double RES;
-
     Lectura(VEC, MAX);
     RES = Suma(VEC, MAX);
-    // Se llama a la función Suma y se almacena el resultado en la variable RES.
-
     printf("\n\nSuma del arreglo: %.2lf", RES);
 }
 
 void Lectura(float A[], int T)
-// La función Lectura se utiliza para leer un arreglo unidimensional de T
-// elementos de tipo real.
 {
     int I;
     for (I = 0; I < T; I++)
@@ -36,8 +26,6 @@ void Lectura(float A[], int T)
 }
 
 double Suma(float A[], int T)
-// La función Suma se utiliza para calcular la suma del cuadrado de los
-// componentes de un arreglo unidimensional de T elementos de tipo real.
 {
     int I;
     double AUX = 0.0;
