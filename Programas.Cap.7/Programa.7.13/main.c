18b6a4da410cdb8b4e8c1201d1a97cff8137d746
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:57:05 -0400

    Update main.c

--- Programas.Cap.7/Programa.7.13/main.c
+++ Programas.Cap.7/Programa.7.13/main.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
+int cuenta(char *); 
 
-/* Calcula longitud.
-   El programa calcula la longitud de la cadena sin utilizar la función strlen. */
-
-int cuenta(char *);  /* Prototipo de función */
-
-int main(void)
+void main(void)
 {
     int i;
     char cad[50];
-
     printf("\nIngrese la cadena de caracteres: ");
     gets(cad);
-
     i = cuenta(cad);
-
     printf("\nLongitud de la cadena: %d", i);
 
     return 0;
 }
 
 int cuenta(char *cadena)
-/* La función calcula la longitud de la cadena. */
 {
     int c = 0;
-    while (cadena[c] != '\0')  // Corrección: condición lógica adecuada
+    while (cadena[c] != '\0')
         c++;
     return c;
 }
