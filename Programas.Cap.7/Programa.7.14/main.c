40cf71e41917b2961a4957c65ca175f3e6d7c6a3
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:57:38 -0400

    Update main.c

--- Programas.Cap.7/Programa.7.14/main.c
+++ Programas.Cap.7/Programa.7.14/main.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 
-/* Calcula longitud en forma recursiva.
-   El programa calcula de manera recursiva la longitud de la cadena sin utilizar
-   la función strlen. */
-
-int cuenta(char *);  /* Prototipo de función */
-
+int cuenta(char *);  
 int main(void)
 {
     int i;
     char cad[50];
-
     printf("\nIngrese la cadena de caracteres: ");
     gets(cad);
-
     i = cuenta(cad);
-
     printf("\nLongitud de la cadena: %d", i);
-
     return 0;
 }
 
 int cuenta(char *cadena)
-/* Esta función calcula la longitud de la cadena en forma recursiva. */
 {
     if (cadena[0] == '\0')
         return 0;
