61fb09bc868b2bc926882b55c9f5448cbf41a157
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:27:12 -0400

    Update main.c

--- Problemas.PS.3.10-PS.3.20/Programa.3.16/main.c
+++ Problemas.PS.3.10-PS.3.20/Programa.3.16/main.c
@@ -3,10 +3,9 @@
 int main() {
     int N;
 
-    printf("Ingrese un número entero positivo: ");
+    printf("Ingrese un nÃºmero entero positivo: ");
     scanf("%d", &N);
 
-    // Parte creciente (1 hasta N)
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= i; j++) {
             printf("%d ", j);
@@ -14,7 +13,6 @@ int main() {
         printf("\n");
     }
 
-    // Parte decreciente (N-1 hasta 1)
     for (int i = N - 1; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
             printf("%d ", j);
