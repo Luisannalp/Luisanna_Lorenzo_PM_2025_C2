ac68bc200674a5680a167ab6325eec26cebf8c96
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:45:27 -0400

    Update main.c

--- Programas-cap-2/Programa2.12/main.c
+++ Programas-cap-2/Programa2.12/main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-/* Igualdad de expresiones.
-El programa, al recibir como datos T, P y N, comprueba la igualdad de
-una expresion determinada.
-
-T, P y N: variables de tipo entero.*/
-
 void main(void)
 {
     int T, P, N;
