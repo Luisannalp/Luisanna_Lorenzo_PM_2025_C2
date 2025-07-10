0778e7c2c2a29076d65c855428b7e4721975ae32
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:35:09 -0400

    Update main.c

--- Programas.Cap.5/Programa.5.9/main.c
+++ Programas.Cap.5/Programa.5.9/main.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-
-/* Búsqueda secuencial en arreglos desordenados. */
-
-#define MAX 100  // Se define el tamaño máximo del arreglo
-
-// Prototipos de funciones
+#define MAX 100  // Se define el tamaÃ±o mÃ¡ximo del arreglo
 void Lectura(int A[], int T);
 int Busca(int A[], int T, int K);
 
 int main(void)
 {
     int RES, ELE, TAM, VEC[MAX];
-
-    // Verificar que el tamaño del arreglo sea correcto
     do {
-        printf("Ingrese el tamaño del arreglo: ");
+        printf("Ingrese el tamaÃ±o del arreglo: ");
         scanf("%d", &TAM);
     } while (TAM > MAX || TAM < 1);
 
@@ -22,13 +15,10 @@ int main(void)
 
     printf("\nIngrese el elemento a buscar: ");
     scanf("%d", &ELE);
-
-    // Se llama a la función que busca en el arreglo
     RES = Busca(VEC, TAM, ELE);
 
     if (RES)
-        // Si RES tiene un valor diferente de 0, se escribe la posición
-        printf("\nEl elemento se encuentra en la posición %d", RES);
+        printf("\nEl elemento se encuentra en la posiciÃ³n %d", RES);
     else
         printf("\nEl elemento no se encuentra en el arreglo");
 
@@ -36,7 +26,6 @@ int main(void)
 }
 
 void Lectura(int A[], int T)
-// La función Lectura se utiliza para leer un arreglo unidimensional
 {
     int I;
     for (I = 0; I < T; I++) {
@@ -46,8 +35,6 @@ void Lectura(int A[], int T)
 }
 
 int Busca(int A[], int T, int K)
-// Esta función localiza en el arreglo un elemento determinado.
-// Si se encuentra, devuelve su posición (empezando desde 1). Si no, devuelve 0.
 {
     int I = 0, BAN = 0, RES;
 
@@ -59,7 +46,7 @@ int Busca(int A[], int T, int K)
     }
 
     if (BAN)
-        RES = I + 1;  // Se suma 1 porque el arreglo inicia en índice 0
+        RES = I + 1; 
     else
         RES = 0;
 
