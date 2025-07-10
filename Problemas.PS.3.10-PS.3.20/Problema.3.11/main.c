756f1f334a36b52d1e674e5ce99cb34ad493b50f
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:23:18 -0400

    Update main.c

--- Problemas.PS.3.10-PS.3.20/Problema.3.11/main.c
+++ Problemas.PS.3.10-PS.3.20/Problema.3.11/main.c
@@ -3,39 +3,36 @@
 int main()
 {
     int N;
-    printf("Ingrese el número de años: ");
+    printf("Ingrese el nÃºmero de aÃ±os: ");
     scanf("%d", &N);
 
-    float VIN[N][4];  // Matriz de N años por 4 tipos de vino
-    float totalTipo[4] = {0};  // Total por tipo de vino
-    float totalAnual[N];       // Total por año
+    float VIN[N][4]; 
+    float totalTipo[4] = {0};  
+    float totalAnual[N];     
 
-    // Leer los datos
     for (int i = 0; i < N; i++)
     {
-        printf("Año %d:\n", i + 1);
+        printf("AÃ±o %d:\n", i + 1);
         totalAnual[i] = 0;
         for (int j = 0; j < 4; j++)
         {
             printf("  Litros tipo %d: ", j + 1);
             scanf("%f", &VIN[i][j]);
-            totalTipo[j] += VIN[i][j];      // Sumar al tipo
-            totalAnual[i] += VIN[i][j];     // Sumar al año
+            totalTipo[j] += VIN[i][j];    
+            totalAnual[i] += VIN[i][j];     
         }
     }
-
-    // Mostrar totales por tipo de vino
+   
     printf("\nTotal producido por tipo de vino:\n");
     for (int j = 0; j < 4; j++)
     {
         printf("Tipo %d: %.2f litros\n", j + 1, totalTipo[j]);
     }
 
-    // Mostrar totales por año
-    printf("\nTotal producido por año:\n");
+    printf("\nTotal producido por aÃ±o:\n");
     for (int i = 0; i < N; i++)
     {
-        printf("Año %d: %.2f litros\n", i + 1, totalAnual[i]);
+        printf("AÃ±o %d: %.2f litros\n", i + 1, totalAnual[i]);
     }
 
     return 0;
