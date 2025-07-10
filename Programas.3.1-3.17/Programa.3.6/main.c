c0e0f9867661820c7b57aa783f7e313a0166dbf7
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 18:53:52 -0400

    Update main.c

--- Programas.3.1-3.17/Programa.3.6/main.c
+++ Programas.3.1-3.17/Programa.3.6/main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-/* Nómina de profesores.
-El programa, al recibir como datos los salarios de los profesores de una
-universidad, obtiene la nómina y el promedio de los salarios.
-
-I: variable de tipo entero.
-SAL, NOM y PRO: variables de tipo real. */
 
 void main(void)
 {
     int I = 0;
     float SAL, PRO, NOM = 0;
     printf("Ingrese el salario del profesor:\t");
-    /* Observa que al menos se necesita ingresar el salario de un profesor para que
-    no ocurra un error de ejecución del programa. */
     scanf("%f", &SAL);
     do
     {
@@ -23,5 +15,5 @@ void main(void)
     }
     while(SAL);
     PRO = NOM / I;
-    printf("\nNómina: %.2f \t Promedio de salarios: %.2f", NOM, PRO);
+    printf("\nNÃ³mina: %.2f \t Promedio de salarios: %.2f", NOM, PRO);
  }
