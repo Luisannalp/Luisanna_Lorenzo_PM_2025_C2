165a5639b9a0fec29e6facaffe1990249f20f09c
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 19:03:49 -0400

    Update main.c

--- Programas.Cap.4/Programa.4.14/main.c
+++ Programas.Cap.4/Programa.4.14/main.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-/* Rango de calificaciones.
-El programa, al recibir como dato una serie de calificaciones, obtiene
-el rango en el que se encuentran.*/
 
-void Rango(int);          /* Prototipo de función. */
+void Rango(int); 
 
 int RA1 = 0;
 int RA2 = 0;
-int RA3 = 0;             /* Variables globales de tipo entero. */
+int RA3 = 0;          
 int RA4 = 0;
 int RA5 = 0;
-/* El uso de variables globales no es muy recomendable. En estos
-problemas se utilizan únicamente con el objetivo de que el lector
-pueda observar la forma en que se aplican. */
 
 void main(void)
 {
     float CAL;
-    printf("Ingresa la primera calificación del alumno:");
+    printf("Ingresa la primera calificaciÃ³n del alumno:");
     scanf("%f", &CAL);
     while (CAL != -1)
     {
-        Rango(CAL);   /* Llamada a la función Rango. Se pasa un parámetro
-                       por valor. */
-        printf("Ingresa la siguiente calificación del alumno:");
+        Rango(CAL);   
+        printf("Ingresa la siguiente calificaciÃ³n del alumno:");
         scanf("%f", &CAL);
     }
     printf("\n0..3.99 = %d", RA1);
@@ -33,8 +26,6 @@ void main(void)
     printf("\n9..10 = %d", RA5);
 }
 void Rango(int VAL)
-/* La función incrementa una variable dependiendo del valor del
-parámetro VAL. */
 {
     if (VAL < 4)
         RA1++;
