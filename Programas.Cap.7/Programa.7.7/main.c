98256ace0e1714b53107a9d52c22ebeb9f257f4c
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-10 20:12:20 -0400

    Update main.c

--- Programas.Cap.7/Programa.7.7/main.c
+++ Programas.Cap.7/Programa.7.7/main.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Otras funciones de la biblioteca string.h para el manejo de cadenas. */
-int main(void)
+void main(void)
 {
     int i;
-    char cad0[20] = "Hola México";
+    char cad0[20] = "Hola MÃ©xico";
     char cad1[20] = "Hola Guatemala";
     char cad2[20] = "Hola Venezuela";
-    char cad3[20] = "Hola México";
+    char cad3[20] = "Hola MÃ©xico";
     char *c;
     char c3;
 
     i = strcmp(cad0, cad1);
-    /* strcmp: devuelve >0 si cad0 > cad1, <0 si cad0 < cad1, 0 si iguales */
-    printf("\nResultado de la comparación —cad0 y cad1—: %d", i);
+    printf("\nResultado de la comparaciÃ³n â€”cad0 y cad1â€”: %d", i);
 
     i = strcmp(cad0, cad2);
-    printf("\nResultado de la comparación —cad0 y cad2—: %d", i);
+    printf("\nResultado de la comparaciÃ³n â€”cad0 y cad2â€”: %d", i);
 
     i = strcmp(cad0, cad3);
-    printf("\nResultado de la comparación —cad0 y cad3—: %d", i);
+    printf("\nResultado de la comparaciÃ³n â€”cad0 y cad3â€”: %d", i);
 
     i = strlen(cad0);
-    /* strlen: longitud de la cadena */
     printf("\nLongitud cadena cad0: %d", i);
 
     i = strlen(cad1);
     printf("\nLongitud cadena cad1: %d", i);
 
-    c = strchr(cad1, 'G');  // busca caracter 'G' en cad1
+    c = strchr(cad1, 'G');  
     if (c != NULL)
     {
         c3 = *c;
         printf("\nEl valor de c3 es:  %c", c3);
     }
 
-    c = strchr(cad2, 'V');  // busca caracter 'V' en cad2
+    c = strchr(cad2, 'V'); 
     if (c != NULL)
     {
         c3 = *c;
