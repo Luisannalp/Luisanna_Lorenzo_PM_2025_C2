56ec2177c62a760e9a4af8bf670b42faa41621c9
Luisannalp <Paulinoluisanna02@gmail.com>
2025-07-13 16:20:49 -0400

    Delete Programa1.4 directory

--- Programa1.4/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(void)
-{
-    float BAS, ALT, SUP;
-    printf("Ingrese la base y la altura del triángulo:");
-    scanf("%f %f", &BAS, &ALT);
-    SUP = BAS * ALT / 2;
-    printf("\nLa superficie del triángulo es: %5.2f", SUP);
-}
